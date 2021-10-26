64a415dfb6683af0ee557c1a3870e8ad6927572c
augustus0825 <77428770+augustus0825@users.noreply.github.com>
2021-10-26 13:04:38 -0400

    Added UI code

---
+++ GUI.py
@@ -0,0 +1,33 @@
+from guizero import App, PushButton, TextBox, Text
+
+
+def count():
+    character_count.value = len(text_box.value)
+
+def show():
+    text_box.show()
+
+def hide():
+    text_box.hide()
+
+def enable():
+    text_box.enable()
+
+def disable():
+    text_box.disable()
+
+
+#Name of the app
+app = App(title ="GUI")
+
+text_box = TextBox(app, command=count)
+character_count = Text(app)
+
+show_button = PushButton(app, text="show", command=show)
+hide_button = PushButton(app, text="hide", command=hide)
+enable_button = PushButton(app, text="enable", command=enable)
+disable_button = PushButton(app, text="disable",command=disable)
+
+
+
+app.display()
